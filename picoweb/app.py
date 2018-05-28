048d79d6206141afa0f282d28800aa323ea3d527
j0ono0 <john@johnnewall.com>
2018-05-28 13:47:11 +0930

    picoweb version up-to-date with flask version. All basic screens displayed

--- picoweb/app.py
+++ picoweb/app.py
@@ -1,18 +1,39 @@
 import picoweb
+import ure as re
 
+# Temporary data
+import temp_data
+networks = temp_data.networks()
 
 app = picoweb.WebApp(__name__)
 
 @app.route("/")
 def index(req, resp):
     yield from picoweb.start_response(resp)
-    yield from resp.awrite("I can show you a table of <a href='squares'>squares</a>.")
+    yield from app.render_template(resp, "index.html")
 
-@app.route("/menu")
-def squares(req, resp):
+@app.route("/networks")
+def menu(req, resp):
     yield from picoweb.start_response(resp)
-    yield from app.render_template(resp, "menu.html", (req,))
+    yield from app.render_template(resp, "networks.html", (networks,))
 
+@app.route(re.compile('^/network/([0-9]+)'))
+def network_details(req, resp):
+    nid = int(picoweb.utils.unquote_plus(req.url_match.group(1)))
+    for network in networks:
+        if network.id == nid:
+            yield from picoweb.start_response(resp)
+            yield from app.render_template(resp, "network_details.html", (network,))
+
+@app.route("/logging")
+def logging(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from app.render_template(resp, "logging.html")
+
+@app.route("/device")
+def device(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from app.render_template(resp, "device.html",(temp_data.config, temp_data.version, temp_data.latest))
 
 import logging
 logging.basicConfig(level=logging.INFO)
